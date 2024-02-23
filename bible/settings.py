f11935abf5e433b96ae9b8b4b7f0c5d9e2ed7fa9
kyungjunkang <kangkyungjun88@gmail.com>
2024-02-23 15:55:54 +0900

    2402231555

--- bible/settings.py
+++ bible/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT= os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://primebible.shop", "https://www.primebible.shop"]
